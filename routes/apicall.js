f0428cc4838db6fbe8195568e59e25ccefd40613
Jeremiah Stafford <jstafford@slmdevice.com>
2019-11-16 13:59:09 -0500

    apicall

--- routes/apicall.js
+++ routes/apicall.js
@@ -1,11 +1,21 @@
 // Include the axios npm package (Don't forget to run "npm install axios" in this folder first!)
 var axios = require("axios");
+const repos = [];
+const logins = [];
+const scores = [];
+const create = [];
+const update = [];
+const sizes = [];
+const langs = [];
+const homes = [];
+
 
 function iterAte(response) {
-  var length = response.items.length;
+  var length = response.data.items.length;
+  var itms = response.data.items;
+  console.log(itms);
   for (i = 0; i < length; i++) {
 
-    var itms = response.items;
 
     logins.push(itms[i].owner.login);
     scores.push(itms[i].score);
@@ -18,55 +28,28 @@ function iterAte(response) {
   }
 };
 
+// var proj key = ${search-term}
+const projkey = "Giftastic";
 // Then run a request with axios to the OMDB API with the movie specified
 async function callApi() {
-  const j = 1;
-  const ans1 = await axios.get("https://api.github.com/search/repositories?q=" + projkey + "&page=" + j)
+  let j = 1;
+  for (j = 1; j <= 8; j++) {
+    let ans;
+    try {
+      if (j == 1) {
+        ans = await axios.get("https://api.github.com/search/repositories?q=" + projkey + "&page=" + j);
+        // var tcount = response.total_count;
+        // $(".total_count").text("# Repos: " + tcount);
 
-  j++
-  tcount = response.total_count;
-  $(".total_count").text("# Repos: " + tcount);
-  iterAte(ans1);
-  const ans2 = await axios.get("https://api.github.com/search/repositories?q=" + projkey + "&page=" + j + "; rel='next'");
-  j++
-  iterAte(ans2);
-  const ans3 = await axios.get("https://api.github.com/search/repositories?q=" + projkey + "&page=" + j + "; rel='next'");
-  j++
-  iterAte(ans3);
-  const ans4 = await axios.get("https://api.github.com/search/repositories?q=" + projkey + "&page=" + j + "; rel='next'");
-  j++
-  iterAte(ans4);
-  const ans5 = await axios.get("https://api.github.com/search/repositories?q=" + projkey + "&page=" + j + "; rel='next'");
-  j++
-  iterAte(ans5);
-  const ans6 = await axios.get("https://api.github.com/search/repositories?q=" + projkey + "&page=" + j + "; rel='next'");
-  j++
-  iterAte(ans6);
-  const ans7 = await axios.get("https://api.github.com/search/repositories?q=" + projkey + "&page=" + j + "; rel='next'");
-  j++
-  iterAte(ans7);
-  const ans8 = await axios.get("https://api.github.com/search/repositories?q=" + projkey + "&page=" + j + "; rel='next'");
-  iterAte(ans8);
-}
-  
+      }
+      else {
+        ans = await axios.get("https://api.github.com/search/repositories?q=" + projkey + "&page=" + j + "; rel='next'");
+      }
+      iterAte(ans);
+    } catch (error) { console.log(error) }
+  }
+
+};
+
 callApi();
-    // .catch(function (error) {
-    //   if (error.response) {
-    //     // The request was made and the server responded with a status code
-    //     // that falls out of the range of 2xx
-    //     console.log("---------------Data---------------");
-    //     console.log(error.response.data);
-    //     console.log("---------------Status---------------");
-    //     console.log(error.response.status);
-    //     console.log("---------------Status---------------");
-    //     console.log(error.response.headers);
-    //   } else if (error.request) {
-    //     // The request was made but no response was received
-    //     // `error.request` is an object that comes back with details pertaining to the error that occurred.
-    //     console.log(error.request);
-    //   } else {
-    //     // Something happened in setting up the request that triggered an Error
-    //     console.log("Error", error.message);
-    //   }
-    //   console.log(error.config);
-    // });
+console.log(logins);
