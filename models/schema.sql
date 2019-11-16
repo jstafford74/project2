c031f34bca7591b57b7b9de5c0e7d1cad67e6db0
NJIB <njibuck@gmail.com>
2019-11-16 12:02:27 -0500

    Database schema updates

--- models/schema.sql
+++ models/schema.sql
@@ -1,5 +1,23 @@
-DROP DATABASE IF EXISTS exampledb;
-CREATE DATABASE exampledb;
+### Schema
 
-DROP DATABASE IF EXISTS testdb;
-CREATE DATABASE testdb;
+DROP DATABASE IF EXISTS repomaster_db;
+CREATE DATABASE repomaster_db;
+
+-- USE DATABASE repomaster_db;
+
+-- CREATE TABLE github_data
+-- (
+-- 	id int NOT NULL AUTO_INCREMENT,
+-- 	html_url varchar(255) NOT NULL,
+-- 	description varchar(255),
+--   updated_at XXXXX NOT NULL,
+--   created_at XXXXX NOT NULL,
+--   language varchar(255) NOT NULL,
+--   forks_count SMALLINT,
+--   score XXXXXXX,
+--   owner_id BIGINT,
+--   owner_login varchar(255),
+--   owner_avatar_url varchar(255),
+--   owner_url varchar(255),
+-- 	PRIMARY KEY (id)
+-- );
