7c13938bcc2bdefd16dfdc74463f8a5c1bcf2278
Ivor Wallace <irwallacejr@gmail.com>
2019-11-23 15:24:35 -0500

    Added table sort and clearing the table.

--- public/js/index.js
+++ public/js/index.js
@@ -30,6 +30,7 @@ function displayResults() {
     url: queryURL,
     method: "GET"
   }).then(function(response) {
+    $("#repo-table tr>td").remove();
     console.log("-----RESULTS-------------");
     for (var loop = 0; loop < response.items.length; loop++) {
       var repoName = response.items[loop].name;
@@ -61,4 +62,50 @@ function displayResults() {
       $("#repo-table").append($newRow);
     }
   });
+}
+
+// eslint-disable-next-line no-unused-vars
+function sortTable(n) {
+  var table,
+    rows,
+    switching,
+    i,
+    x,
+    y,
+    shouldSwitch,
+    dir,
+    switchcount = 0;
+  table = document.getElementById("repo-table");
+  switching = true;
+  dir = "asc";
+  while (switching) {
+    switching = false;
+    rows = table.rows;
+    for (i = 1; i < rows.length - 1; i++) {
+      shouldSwitch = false;
+      x = rows[i].getElementsByTagName("TD")[n];
+      y = rows[i + 1].getElementsByTagName("TD")[n];
+      if (dir === "asc") {
+        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+          shouldSwitch = true;
+          break;
+        }
+      } else if (dir === "desc") {
+        if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+          shouldSwitch = true;
+          break;
+        }
+      }
+    }
+    if (shouldSwitch) {
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+      switchcount++;
+    } else {
+      if (switchcount === 0 && dir === "asc") {
+        dir = "desc";
+        switching = true;
+      }
+    }
+  }
 }
