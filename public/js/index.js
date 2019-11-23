43094a47f81e0761dca84c0f9c37403f3c91c6d3
Rachael Clark <rachael.clark@uconn.edu>
2019-11-23 12:23:08 -0500

    updated

--- public/js/index.js
+++ public/js/index.js
@@ -7,31 +7,6 @@ $submitProject.on("click", function(event) {
     .val()
     .trim();
   console.log(searchitem);
-});
-
-// NJIB added the following code, to populate response results
-function renderRepoTable(logRepos) {
-
-  // Store everything into a variable.
-  let repoName = logRepos.itms[0].repoName;
-  let ownerLogin = logRepos.itms[0].owner.name;
-  let updateDt = logRepos.itms[0].updated_at;
-  let htmlURL = logRepos.itms[0].homepage;
-  let starRepo = "<i class='icon-star-empty'></i>";
-
-  // Create the new row
-  var newRow = $("<tr>").append(
-    $("<td class='repoName0'>").text(repoName),
-    $("<td class='ownerLogin0'>").text(ownerLogin),
-    $("<td class='updateDt0'>").text(updateDt),
-    $("<td class = 'htmlURL0'>").text(htmlURL),
-    $("<td class = 'starRepo0'>").innerHTML(starRepo)
-  );
-
-  // Append the new row to the table
-  $("#repo-table > tbody").append(newRow);
-};
-
 
   //Input Validation//
   if (searchitem === "") {
@@ -44,6 +19,7 @@ function renderRepoTable(logRepos) {
   displayResults();
 });
 
+//Function to Display Search Results//
 function displayResults() {
   var queryURL = "https://api.github.com/search/repositories?q=" + searchitem;
 
