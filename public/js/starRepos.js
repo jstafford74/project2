9ec7f3d7fe970d8b3daac4d1f99952124a1b4ec0
NJIB <njibuck@gmail.com>
2019-11-25 23:14:54 -0500

    GET working - logging to console, not screen

--- public/js/starRepos.js
+++ public/js/starRepos.js
@@ -7,7 +7,7 @@ const func = (index) => console.log(index)
 
 // The API object contains methods for each kind of request we'll make
 var API = {
-  starRepo: function(repo) {
+  starRepo: function (repo) {
     return $.ajax({
       headers: {
         "Content-Type": "application/json"
@@ -17,13 +17,13 @@ var API = {
       data: JSON.stringify(repo)
     });
   },
-  getRepos: function() {
+  getRepos: function () {
     return $.ajax({
       url: "api/repos",
       type: "GET"
     });
   },
-  unstarRepo: function(id) {
+  unstarRepo: function (id) {
     return $.ajax({
       url: "api/repos/" + id,
       type: "DELETE"
@@ -33,14 +33,15 @@ var API = {
 
 // handleStarRepo is called whenever we submit a new repo
 // Save the new repo to the db and refresh the list
-var handleStarRepo = function(event, index) {
+var handleStarRepo = function (event, index) {
   event.preventDefault();
   console.log(typeof event.srcElement.className, event.srcElement.className)
   const tableRow = event.srcElement.parentNode.parentNode.childNodes
-  for(let i = 0; i < tableRow.length; i++) {
+  console.log("tableRow: ", tableRow);
+  for (let i = 0; i < tableRow.length; i++) {
     console.log(tableRow[i].innerText)
   }
-  if(event.srcElement.className.includes('far')) {
+  if (event.srcElement.className.includes('far')) {
     $(`#star-${index}`).removeClass("far");
     $(`#star-${index}`).addClass("fas");
     var repo = {
@@ -49,20 +50,31 @@ var handleStarRepo = function(event, index) {
       ownerLogin: tableRow[1].innerText,
       updateAt: tableRow[2].innerText,
     };
-  
-    API.starRepo(repo).then(function() {
+
+    API.starRepo(repo).then(function () {
     });
   } else {
     console.log("Unstar button clicked!");
     $(`#star-${index}`).removeClass("fas");
     $(`#star-${index}`).addClass("far");
-  
+
     var idToDelete = index;
     console.log("index: ", index);
-  
-    API.unstarRepo(idToDelete).then(function() {
+
+    API.unstarRepo(idToDelete).then(function () {
     });
   }
-    
+
 }
+
+var retrieveSavedRepos = function (event) {
+  event.preventDefault();
+  API.getRepos().then(function (response) {
+    for (var i = 0; i < response.length; i++) {
+    console.log(response[i]);
+    }
+  });
+}
+
+
 
